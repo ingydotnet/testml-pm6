3713be50dcc2eb1dd787fd1214d05166721c720a
Ingy dot Net <ingy@ingy.net>
2010-06-23 21:01:30 -0700

    Copy Meta section grammar from the Spec.

--- lib/TestML/Parser/Grammar.pm
+++ lib/TestML/Parser/Grammar.pm
@@ -3,43 +3,95 @@ use v6;
 # http://perlcabal.org/syn/S05.html
 
 grammar TestML {
-    rule TOP { <document> }
+    regex TOP { <document> }
 
-    rule ANY       { . }                 # Any unicode character
-    rule SPACE     { <[\ \t]> }          # A space or tab character
-    rule BREAK     { \n }                # A newline character
-    rule EOL       { \r? \n }            # A Unix or DOS line ending
-    rule NON-BREAK { . }                 # Any character except newline
-    rule LOWER     { <[a..z]> }          # Lower case ASCII alphabetic character
-    rule UPPER     { <[A..Z]> }          # Upper case ASCII alphabetic character
-    rule ALPHANUM  { <[A..Za..z0..9]> }  # ASCII alphanumeric character
-    rule WORD      { <[A..Za..z0..9_]> } # A ``word'' character
-    rule DIGIT     { <[0..9]> }          # A numeric digit
-    rule STAR      { '*' }               # An asterisk
-    rule DOT       { '.' }               # A period character
-    rule HASH      { '#' }               # An octothorpe (or hash) character
-    rule BACK      { '\\' }              # A backslash character
-    rule SINGLE    { "'" }               # A single quote character
-    rule DOUBLE    { '"' }               # A double quote character
-    rule ESCAPE    { <[0nt]> }           # One of the escapable character IDs 
+    regex ANY       { . }                 # Any unicode character
+    regex SPACE     { <[\ \t]> }          # A space or tab character
+    regex BREAK     { \n }                # A newline character
+    regex EOL       { \r? \n }            # A Unix or DOS line ending
+    regex NON-BREAK { . }                 # Any character except newline
+    regex LOWER     { <[a..z]> }          # Lower case ASCII alphabetic character
+    regex UPPER     { <[A..Z]> }          # Upper case ASCII alphabetic character
+    regex ALPHANUM  { <[A..Za..z0..9]> }  # ASCII alphanumeric character
+    regex WORD      { <[A..Za..z0..9_]> } # A ``word'' character
+    regex DIGIT     { <[0..9]> }          # A numeric digit
+    regex STAR      { '*' }               # An asterisk
+    regex DOT       { '.' }               # A period character
+    regex HASH      { '#' }               # An octothorpe (or hash) character
+    regex BACK      { '\\' }              # A backslash character
+    regex SINGLE    { "'" }               # A single quote character
+    regex DOUBLE    { '"' }               # A double quote character
+    regex ESCAPE    { <[0nt]> }           # One of the escapable character IDs 
 
-    rule document {
+    regex document {
         <meta-section> <test-section> <data-section>?
     }
 
-    rule meta-section { [ <comment> | <blank-line> ]* <meta-testml-statement> }
+    regex meta-section {
+        [ <comment> | <blank-line> ]*
+        <meta-testml-statement>
+        [ <meta-statement> | <comment> | <blank-line> ]*
+    }
 
-    rule comment { ^^ '#' \N* \n }
-    rule blank-line { ^^ \s*? \n }
+    regex comment { HASH <line> }
 
+    regex line { <NON-BREAK>* <EOL> }
 
-    rule test-section { }
-    rule data-section { }
+    regex blank-line { <SPACE>* <EOL> }
 
+    regex meta-testml-statement {
+        '%TestML:' <SPACE>+ <testml-version> [ <SPACE>+ <comment> | <EOL> ]
+    }
 
-    rule SPACE { <[\ \t]> }
+    regex testml-version { <DIGIT> <DOT> <DIGIT>+ }
 
-    rule meta-testml-statement { '%TestML:' <SPACE>+ <testml-version> [ <SPACE>+ <comment> | <EOL> ] }
-    rule testml-version { <DIGIT> <DOT> <DIGIT>+ }
+    regex meta-statement {
+        '%' meta-keyword ':' <SPACE>+ <meta-value>
+        [<SPACE>+ <comment> | <EOL>]
+    }
+
+    regex meta-keyword { <core-meta-keyword> | <user-meta-keyword> }
+
+    regex core-meta-keyword {
+        [ Title | Data | Plan | BlockMarker | PointMarker ]
+    }
+
+    regex user-meta-keyword { <LOWER> <WORD>* }
+
+    regex meta-value { [ <quoted-string> | <unquoted-string> ] }
+
+    regex quoted-string { <single-quoted-string> | <double-quoted-string> }
+
+    regex single-quoted-string {
+        <SINGLE>
+        [
+            [<ANY> - [<BREAK> | <BACK> | <SINGLE>]] |
+            <BACK> <SINGLE> |
+            <BACK> <BACK>
+        ]*
+        <SINGLE>
+    }
+
+    regex double-quoted-string {
+        <DOUBLE>
+        [
+            [<ANY> - [<BREAK> | <BACK> | <DOUBLE>]] |
+            <BACK> <DOUBLE> |
+            <BACK> <BACK> |
+            <BACK> <ESCAPE>
+        ]*
+        <DOUBLE>
+    }
+
+    regex unquoted-string {
+        [<ANY> - [<SPACE> | <BREAK> | <HASH>]]
+        [
+            [<ANY> - [<BREAK> | <HASH>]]*
+            [<ANY> - [<SPACE> | <BREAK> | <HASH>]]
+        ]?
+    }
+
+    regex test-section { <ANY>* }
+
+    regex data-section { <ANY>* }
 }
-
