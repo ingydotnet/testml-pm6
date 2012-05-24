ea57e74a494959d441af7df3a71e0ecde45e6032
Moritz Lenz <moritz@faui2k3.org>
2012-05-24 20:38:32 +0200

    make it compile on rakudo again
    also a small update to exception handling

--- lib/TestML/Document.pm
+++ lib/TestML/Document.pm
@@ -8,65 +8,66 @@ class TestML::Assertion { ... }
 class TestML::Document::Data { ... }
 
 #-----------------------------------------------------------------------------
-class TestML::Document;
-
-has $.meta = TestML::Document::Meta.new;
-has $.test = TestML::Document::Tests.new;
-has $.data = TestML::Document::Data.new;
-
-#-----------------------------------------------------------------------------
-class TestML::Document::Meta;
-
-has $.data = {
-    TestML => '',
-    Data => [],
-    Title => '',
-    Plan => 0,
-    BlockMarker => '===',
-    PointMarker => '---',
-};
+class TestML::Document {
+    has $.meta = TestML::Document::Meta.new;
+    has $.test = TestML::Document::Tests.new;
+    has $.data = TestML::Document::Data.new;
+}
 
 #-----------------------------------------------------------------------------
-class TestML::Document::Tests;
-
-has $.statements = [];
-
-#-----------------------------------------------------------------------------
-class TestML::Statement;
-
-has $.expression = TestML::Expression.new;
-has $.assertion is rw;
-has $.points = [];
-
-#-----------------------------------------------------------------------------
-class TestML::Expression;
-
-has $.transforms = [];
+class TestML::Document::Meta {
+    has $.data = {
+        TestML => '',
+        Data => [],
+        Title => '',
+        Plan => 0,
+        BlockMarker => '===',
+        PointMarker => '---',
+    };
+}
 
 #-----------------------------------------------------------------------------
-class TestML::Assertion;
-
-has $.name is rw;
-has $.expression = TestML::Expression.new;
-
-#-----------------------------------------------------------------------------
-class TestML::Transform;
-
-has $.name;
-has $.args;
+class TestML::Document::Tests {
+    has $.statements = [];
+}
 
 #-----------------------------------------------------------------------------
-class TestML::String is TestML::Transform;
-
-has $.value;
-
-#-----------------------------------------------------------------------------
-class TestML::Document::Data;
-
-has $.blocks = [];
+class TestML::Statement {
+    has $.expression = TestML::Expression.new;
+    has $.assertion is rw;
+    has $.points = [];
+}
 
 #-----------------------------------------------------------------------------
-class TestML::Block;
+class TestML::Expression {
+    has $.transforms = [];
+}
 
-has $.label is rw = '';
-has $.points = {};
+#-----------------------------------------------------------------------------
+class TestML::Assertion {
+    has $.name is rw;
+    has $.expression = TestML::Expression.new;
+}
+
+#-----------------------------------------------------------------------------
+class TestML::Transform {
+    has $.name;
+    has $.args;
+}
+
+#-----------------------------------------------------------------------------
+class TestML::String is TestML::Transform {
+    has $.value;
+}
+
+#-----------------------------------------------------------------------------
+class TestML::Document::Data {
+    has $.blocks = [];
+}
+
+#-----------------------------------------------------------------------------
+class TestML::Block {
+    has $.label is rw = '';
+    has $.points = {};
+}
+
